cfe9ff4534fa964ec6ea0df2a365598de28e2f78
blthedfo <60155491+blthedfo@users.noreply.github.com>
2020-02-27 13:35:42 -0600

    commiting original code to Sprint2SignIn Branch
    validateForm Function completed,
    Form for Employee ID and password completed

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -4,5 +4,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function validateForm() {
 	// TODO: Validate the user input
+	var x = document.forms['signInForm'].employee.value;
+	var y = document.forms['signInForm'].password.value;
+	    if (x == "" || y == ""){
+            if (x == "" && y == ""){
+                text = "The username field must not be empty. The password field must not be empty.";
+                document.getElementById("validation").innerHTML = text;
+            }
+            else if (x == ""){
+                text = "The username field must not be empty. "
+                document.getElementById("validation").innerHTML = text;
+            }
+            else if (y == ""){
+                text = "The password field must not be empty. ";
+                document.getElementById("validation").innerHTML = text;
+            }
+            document.getElementById("validation").style.visibility = "visible";
+            return false;
+        }
 	return true;
 }
