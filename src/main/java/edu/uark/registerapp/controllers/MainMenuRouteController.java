55e464c8c9f6bdf645c7eaafce76b6aff4a152a1
RylanMcA <55506412+RylanMcA@users.noreply.github.com>
2020-02-28 14:23:02 -0600

    Reverting
    Changing things but setting the value to false

--- src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/MainMenuRouteController.java
@@ -40,7 +40,7 @@ public class MainMenuRouteController extends BaseRouteController {
 		// TODO: Examine the ActiveUser classification if you want this information
 			modelAndView.addObject(
 				ViewModelNames.IS_ELEVATED_USER.getValue(),
-				this.isElevatedUser(activeUserEntity.get()));
+				false);
 				//this.isElevatedUser(activeUserEntity.get())
 		
 		return modelAndView;
