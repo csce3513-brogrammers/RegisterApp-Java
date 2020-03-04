22d5ba53a011caeb60061b7ed26fe4738edc1bec
Chase-Zimmerman <60163523+Chase-Zimmerman@users.noreply.github.com>
2020-03-03 18:16:26 -0600

    Update ActiveEmployeeExistsQuery.java

--- src/main/java/edu/uark/registerapp/commands/employees/helpers/ActiveEmployeeExistsQuery.java
+++ src/main/java/edu/uark/registerapp/commands/employees/helpers/ActiveEmployeeExistsQuery.java
@@ -1,22 +1,19 @@
 package edu.uark.registerapp.commands.employees.helpers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
 public class ActiveEmployeeExistsQuery {
 
-    private static EmployeeRepository employee;
-
-    public ActiveEmployeeExistsQuery(EmployeeRepository employeeRep) {
-
-        employee = employeeRep;
-    }
-
     public void query() {
 
-        if (employee.existsByIsActive(false)) {
+        if (employeerepository.existsByIsActive(false)) {
 
             throw new NotFoundException("Employee");
         }
     }
+
+    @Autowired
+    private EmployeeRepository employeerepository;
 }
