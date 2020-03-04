91630ce1577c28dc05b2ab7f1d472ecc1fa77191
blthedfo <60155491+blthedfo@users.noreply.github.com>
2020-03-04 12:41:53 -0600

    Update SignInRouteController.java
    not finished but starting to get somewhere on it.

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -1,24 +1,48 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Map;
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
 
 @Controller
 @RequestMapping(value = "/")
 public class SignInRouteController extends BaseRouteController {
-	// TODO: Route for initial page load
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(@RequestParam Map<String, String> signIn,  HttpServletRequest request) {
+		ModelAndView modelAndView = new ModelAndView(ViewNames.SIGN_IN.getRoute());
+				
+		final Optional<ActiveUserEntity> activeUserEntity =
+			this.getCurrentUser(request);
+		if (!activeUserEntity.isPresent()) {
+			ModelAndView modelAndView2 = new ModelAndView(ViewNames.SIGN_IN.getRoute());
+			//this.buildInvalidSessionResponse();
+			//return this.buildNoPermissionsResponse("/signIn");
+		}
+	
+		modelAndView.addObject(
+			"isElevatedUser",
+			false);
+			//this.isElevatedUser(activeUserEntity.get())
+
+		return modelAndView;		
+	}
 
 	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public ModelAndView performSignIn(
+	public ModelAndView performSignIn(EmployeeSignIn employee, 
 		// TODO: Define an object that will represent the sign in request and add it as a parameter here
-		HttpServletRequest request
+		HttpServletRequest request 
 	) {
 
 		// TODO: Use the credentials provided in the request body
