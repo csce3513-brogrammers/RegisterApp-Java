3dc30f7fb2365524f298b60af5339ff0aaa11c36
Chase-Zimmerman <60163523+Chase-Zimmerman@users.noreply.github.com>
2020-03-04 15:33:00 -0600

    Update ActiveUserDeleteCommand.java

--- src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -1,1 +1,32 @@
+package edu.uark.registerapp.commands.activeUsers;
 
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+import edu.uark.registerapp.models.repositories.ActiveUserRepository;
+
+public class ActiveUserDeleteCommand {
+
+    private String sessionID;
+
+    public ActiveUserDeleteCommand(String session) {
+
+        this.sessionID = session;
+    }
+
+    @Transactional
+    public void deleteActive() {
+
+        Optional<ActiveUserEntity> activeUser = activeRepo.findBySessionKey(sessionID);
+
+        if (activeUser.isPresent()) {
+
+            ActiveUserEntity active = activeUser.get();
+            activeRepo.delete(active);
+        }
+    }
+
+    @Autowired
+    private ActiveUserRepository activeRepo;
+}
